3340436c92e4979fcaad8632f5c9fca5bf60986c
Malcolm Jack <inquisitorjax@gmail.com>
2017-11-01 10:17:20 +0200

    initial project with media plugin

---
+++ Xamarin.MCS.OCR/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Xamarin.MCS.OCR
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new MainViewModel();
+        }
+    }
+}
---
+++ XamarinForms/XamarinForms.Droid/MainActivity.cs
@@ -0,0 +1,24 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Plugin.Permissions;
+
+namespace XamarinForms.Droid
+{
+    [Activity(Label = "XamarinForms", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
+    {
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Permission[] grantResults)
+        {
+            PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new App());
+        }
+    }
+}
